22d4a73475bf67f4f671f453f4df58be41f1db88
Brent Muha <800@holbertonschool.com>
2020-04-08 10:48:38 -0700

    Add remove white

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -4,6 +4,15 @@ Check if a string is a palindrome
 """
 
 
+def remove_white(s):
+    # Remove characters that are not letters
+    new = ''
+    for ch in s:
+        if ord(ch) >= ord('a') and ord(ch) <= ord('z'):
+            new += ch
+    return new
+
+
 def palindrome(s):
     # Check if a string is a palindrome
     if len(s) <= 1:
@@ -14,6 +23,8 @@ def palindrome(s):
         return palindrome(s[1:-1])
 
 if __name__ == '__main__':
-    print(palindrome('lsdkjfskf'))
-    print(palindrome('radar'))
-    print(palindrome('amanaplanacanalpanama'))
+    print(palindrome(remove_white('lsdkjfskf')))
+    print(palindrome(remove_white('radar')))
+    print(palindrome(remove_white('a man, a plan, a canal, panama')))
+    print(palindrome(remove_white('')))
+    print(palindrome(remove_white("madam i'm adam")))
